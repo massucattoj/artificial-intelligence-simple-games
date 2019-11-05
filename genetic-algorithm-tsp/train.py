525b12f0ec97f33a1c5bb6fab1b3bd16c1928253
massucattoj <massucattoj@gmail.com>
2019-11-05 14:17:32 -0300

    Main Loop

--- genetic-algorithm-tsp/train.py
+++ genetic-algorithm-tsp/train.py
@@ -34,7 +34,7 @@ class Route():
 
             self.dna.append(random)
         self.dna.append(0)
-        # dna = 3, 1, 2, 0
+        # ex dna = 3, 1, 2, 0
 
     # Building the crossover method
     ''' Take two DNAs (parents), mix them up and create a new DNA'''
@@ -55,6 +55,94 @@ class Route():
 
 
         # Random partial mutations  (after crossover add a little bit of mutation)
+        # Prevent that our offspring to be too similar to its parents.
+        for i in range(self.dnaLength -1):
+            if np.random.rand() <= 0.1:
+                previous = self.dna[i]
+                rnd_gene = np.random.randint(1, self.dnaLength)
+                inx = self.dna.index(rnd_gene)
+                self.dna[inx] = previous
+                self.dna[i] = rnd_gene
+
+            elif np.random.rand() <= 0.1:
+                rnd = np.random.randint(1, self.dnaLength)
+                prevInx = self.dna.index(rnd)
+                self.dna.insert(i, rnd)
+
+                if i >= prevInx:
+                    self.dna.pop(prevInx)
+                else:
+                    self.dna.pop(prevInx + 1)
+
+
+''' Initializing the main code '''
+populationSize = 50 # how many bots (number of objects of this route class)
+mutationRate = 0.1
+nSelected = 5 # number of bots selected to create a new population
+
+# create an object of the environment class
+env = Environment()
+dnaLength = len(env.planets)
+population = ()
+
+
+# Create the first population
+for i in range(populationSize):
+    route = Route(dnaLength)
+    population.append(route)
+
+# Starting the main loop (iterate over all generations)
+generation = 0
+bestDist = np.inf
+while True:
+    generation += 1
+
+    # Evaluating the population
+    # For each route object in population
+    for route in population:
+        env.reset()
+
+        for i in range(dnaLength):
+            action = route.dna[i]
+            route.distance += env.step(action, 'none') # at the end this variable give the total distance travelled by the rocket
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
